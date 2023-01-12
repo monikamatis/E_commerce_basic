a6ae3af720be2cda63fb69d4334fc2ea47762359
monikamatis <monikamatis@gmail.com>
2023-01-12 15:57:42 +0000

    added Customer model in models.py;

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core.validators import EmailValidator
 
-# Create your models here.
+
+class Customers(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100,
+                                  Null=False,
+                                  help_text='First name.')
+    last_name = models.CharField(max_length=100,
+                                 Null=False,
+                                 help_text='Last name.')
+    email = models.EmailField(max_length=100,
+                              Null=False,
+                              help_text='Please type in your email.')
+    password = models.CharField(max_length=100,
+                                min_length=8,
+                                default=None,
+                                help_text='Minimum length 8 characters')
+    phone = models.CharField(max_length=10,
+                             help_text='Contact number')
+    newsletter = models.BooleanField(default=True,
+                                     help_text='Sign up for our newsletter')
+
+    def register_customer(self):
+        self.save()
+
+    def if_exists(self):
+        if Customers.objects.filter(email=self.email):
+            return True
+        else:
+            return False
