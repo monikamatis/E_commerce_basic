6ff05ccd0f18a437327f7534f0df0640b4da68e8
monikamatis <monikamatis@gmail.com>
2023-01-12 16:11:17 +0000

    changed Customer model in models.py; added path to urls.py in shop; added basic view for shop_home; modified base.html;

--- shop/models.py
+++ shop/models.py
@@ -4,16 +4,12 @@ from django.db import models
 class Customers(models.Model):
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=100,
-                                  Null=False,
                                   help_text='First name.')
     last_name = models.CharField(max_length=100,
-                                 Null=False,
                                  help_text='Last name.')
     email = models.EmailField(max_length=100,
-                              Null=False,
                               help_text='Please type in your email.')
     password = models.CharField(max_length=100,
-                                min_length=8,
                                 default=None,
                                 help_text='Minimum length 8 characters')
     phone = models.CharField(max_length=10,
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def shop_home(request):
+
+    return render(request, 'base.html')
