3b4918e4bb9dc5409afb0c9fdf4ed51051143ec5
monikamatis <monikamatis@gmail.com>
2023-01-13 02:44:18 +0000

    defined view functions for product_list and product_detail in views.py;

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
 
-def shop_home(request):
+def product_list(request, category_slug=None):
+    """
+    Define a product list view for the product catalog.
+    :param request: required parameter of a function defining a view
+    :param category_slug: defaults to None
+    :return: a list of all available products with relevant categories
+        or 'not found' error page.
+    """
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
 
-    return render(request, 'base.html')
+    if category_slug:
+
+        category = get_object_or_404(Category,
+                                     slug=category_slug)
+        products = products.filter(category=category)
+
+    return render(request, 'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+
+# may need to change a parameter in product_detail function if
+# it causes problems ('shadows built-in name')
+def product_detail(request, id, slug):
+    """
+    Define a detailed product view.
+    :param request: required parameter for any view function
+    :param id: product id
+    :param slug: product slug
+    :return: details of a product or 'not found' error page.
+    """
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+
+    return render(request, 'shop/product/detail.html',
+                  {'product': product})
